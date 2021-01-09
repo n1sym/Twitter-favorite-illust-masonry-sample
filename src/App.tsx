350c0b806f1609582fcb73a3902b4e2fff43e3f5
hukurouo <owlbook248@gmail.com>
2021-01-10 01:22:10 +0900

    header tyousei

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       </Helmet>
       <div className="bg-blue-50 min-h-screen">
         <div className="container mx-auto">
-          <header className="flex justify-center items-center text-3xl h-24">
+          <header className="flex justify-center items-center text-3xl h-32 mx-5">
             いいねした画像を並べるサイト
           </header>
           <div className="flex justify-center">
